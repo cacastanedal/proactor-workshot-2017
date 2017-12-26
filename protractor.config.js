7668fb3e1e4b7f3482560f2271c23f20eff6450c
Carlos Castaneda Largacha <ccastanedal@pslcol.com.co>
2017-12-26 15:29:16 -0500

    Buying tshirt test

--- protractor.config.js
+++ protractor.config.js
@@ -1,7 +1,7 @@
 exports.config = {
 	framework: 'jasmine',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['test/Google.spec.js'],
+	specs: ['test/BuyTshirt.spec.js'],
 	onPrepare: () => {
 		browser.ignoreSynchronization = true;
 	}
