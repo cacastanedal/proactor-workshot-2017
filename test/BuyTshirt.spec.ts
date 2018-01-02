753a6ab44703577208bcd8156ee0ddb8816f0017
Carlos Castaneda Largacha <ccastanedal@pslcol.com.co>
2018-01-02 11:56:16 -0500

    Splitting down buy Tshirt test into different describes

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -12,33 +12,78 @@ import { MenuContentPage,
   OrderResumePage
 } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productDetailPage: ProductDetailPage = new ProductDetailPage;
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage;
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage;
-  const signInStepPage: SignInStepPage = new SignInStepPage;
-  const addressStepPage: AddressStepPage = new AddressStepPage;
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage;
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage;
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage;
-  const orderResumePage: OrderResumePage = new OrderResumePage;
-
-  it('then should be bought a t-shirt', async () => {
+describe('Open page in browser',  () => {
+  it('getting a specify URL', async () => {
     await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productListPage.goToTShirtList();
-    await productDetailPage.addToCart();
-    await productAddedModalPage.proceedToCheckout();
-    await summaryStepPage.proceedToCheckout();
-    await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');  
-    await addressStepPage.proceedToCheckout();
-    await shippingStepPage.proceedToCheckout();
-    await paymentStepPage.selectPaymentMethod();
-    await bankPaymentPage.confirmOrder();
+  });
+});
 
+describe('Selecting Tshirt to buy', () => {
+  
+  it('look for Tshirts', async () => {
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    await menuContentPage.goToTShirtMenu();
+  });
+
+  it('select first tshirt', async () => {
+    const productListPage: ProductListPage = new ProductListPage();
+    await productListPage.goToTShirtList();
+  });
+
+  it('add to cart', async () => {
+    const productDetailPage: ProductDetailPage = new ProductDetailPage;
+    await productDetailPage.addToCart();
+  });
+
+  it('proceed to checkout', async () => {
+    const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage;
+    await productAddedModalPage.proceedToCheckout();
+  });
+
+  it('confirm summary', async () => {
+    const summaryStepPage: SummaryStepPage = new SummaryStepPage;
+    await summaryStepPage.proceedToCheckout();
+  });
+});
+
+describe('Logging in', () => {
+  it('typing user and password', async() => {
+    const signInStepPage: SignInStepPage = new SignInStepPage;
+    await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');  
+  });
+});
+
+describe('Confirm the default address', () => {
+  it('proceed to checkout', async () => {
+    const addressStepPage: AddressStepPage = new AddressStepPage;
+    await addressStepPage.proceedToCheckout();
+  });
+});
+
+describe('Shipping options', () => {
+  it('agree with terms and proceed', async () => {
+    const shippingStepPage: ShippingStepPage = new ShippingStepPage;
+    await shippingStepPage.proceedToCheckout();
+  });
+});
+
+describe('Bank payment', async () => {
+  it('pay through wire transfer', async () => {
+    const paymentStepPage: PaymentStepPage = new PaymentStepPage;
+    await paymentStepPage.selectPaymentMethod();
+  });
+
+  it('confirm payment', async () => {
+    const bankPaymentPage: BankPaymentPage = new BankPaymentPage;
+    await bankPaymentPage.confirmOrder();
+  });
+});
+
+describe('Tshirt bought', () => {
+  it('confirmation message', async () => {
+    const orderResumePage: OrderResumePage = new OrderResumePage;
     await expect(orderResumePage.getConfirmationText())
      .toBe('Your order on My Store is complete.');
   });
 });
+
