4c8c7c129f1d1acccc5bfaf0fa85b3e20c392ca0
Carlos Castaneda Largacha <ccastanedal@pslcol.com.co>
2018-01-02 09:29:20 -0500

    Shorter locators

--- src/page/PaymentStep.page.ts
+++ src/page/PaymentStep.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class PaymentStepPage {
   private get paymentMethodOption(): ElementFinder {
-    return $('#HOOK_PAYMENT > div:nth-child(1) > div > p > a');
+    return $('.bankwire');
   }
   
   public selectPaymentMethod(): promise.Promise<void> {
--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class ProductListPage {
   private get tShirtImage(): ElementFinder {
-    return $('ul.product_list > li > div > div.left-block > div > a.product_img_link > img');
+    return $('.product_img_link');
   }
 
   public goToTShirtList(): promise.Promise<void> {
